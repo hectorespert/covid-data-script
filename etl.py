876689ddd9eef2c0ab353fb2484ccbdac02f0c92
Hector Espert <hectorespertpardo@gmail.com>
2020-11-13 23:33:11 +0100

    Test datastore

--- etl.py
+++ etl.py
@@ -13,11 +13,10 @@ with RemoteCKAN('https://dadesobertes.gva.es') as gva:
     package = gva.action.package_show(id='15810be9-d797-4bf3-b37c-4c922bee8ef8')
     for resource in package['resources']:
         sleep(1) # TODO: Sleep or not?
-        print("Reading resource: " + resource['id'])
-        # created_date = resource['created']
-        # last_modified_date = resource['last_modified']
-        # data_date = resource['name'][-10:]
-        # data = gva.action.datastore_search(id=resource['id'])
+        created_date = resource['created']
+        last_modified_date = resource['last_modified']
+        data_date = resource['name'][-10:]
+        data = gva.action.datastore_search(id=resource['id'])
         # for record in data['records']:
         #
         #     if 'Municipi / Municipio' in record:
